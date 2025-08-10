c359602dcd625978e18b7e02a293f76b7bf6039a
Karan Maheshwari <mkaran0015@gmail.com>
2025-08-10 12:48:30 +0000

    Introduction of sources

--- models/staging/stg_division.sql
+++ models/staging/stg_division.sql
@@ -1,3 +1,16 @@
+-- with stg_division as (
+-- Select 
+--     emp_id, 
+--     marks,
+-- case 
+--     when marks >= 90 then 'A' 
+--     when marks >= 80 and marks < 90 then 'B'
+--     else 'C'
+-- END as GRADE 
+-- from employee_table
+-- )
+-- Select * from stg_division
+
 with stg_division as (
 Select 
     emp_id, 
@@ -7,6 +20,6 @@ case
     when marks >= 80 and marks < 90 then 'B'
     else 'C'
 END as GRADE 
-from employee_table
+from {{ source('source_info', 'employee_table') }}
 )
 Select * from stg_division
